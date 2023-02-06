a45c3744b1e8de32429335b4c9e6b404e083b537
Mahmoud Ali Sadeghi <sadeghi.yasser@gmail.com>
2023-02-06 11:20:26 +0330

    add str to developer model

--- DevApp/models.py
+++ DevApp/models.py
@@ -4,3 +4,6 @@ class Developer(models.Model):
     FirstName = models.CharField(max_length=50)
     LastName = models.CharField(max_length=50)
     Years = models.SmallIntegerField(null=True)
+    
+    def __str__(self) -> str:
+        return self.LastName
